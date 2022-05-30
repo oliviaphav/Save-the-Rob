732ed1574a03ad5268fe490c1c3fc64faaf7a69d
oliviaphav <phav.olivia@gmail.com>
2022-05-30 15:46:20 +0200

    Ajout tests

--- main.cpp
+++ main.cpp
@@ -17,5 +17,3 @@ int main()
 
     return 0;
 }
-
-// git add arme.hpp catch.hpp entite.hpp jeu.cpp jeu.hpp joueur.hpp laser.cpp laser.hpp main.cpp Makefile mur.cpp mur.hpp obstacle.hpp porte_arme.cpp porte_arme.hpp robot.cpp robot.hpp support.cpp support.hpp tests_catch_laser.cpp tests_catch_mur.cpp tests_catch_porte_arme.cpp tests_catch_robot.cpp tests_catch_support.cpp
